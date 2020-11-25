0b74eb5acce7a570daec16031ce22491b3014902
Kyle Wilson <kyle@hinterlandstills.com>
2020-11-25 13:07:30 -0600

    commit tree 198fe17cc75a31abf97b2c7a762062cb2a2b4c0c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+  
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  
+
+  def kind
+    if ((a && b && c) > 0) && (a + b > c) && (a + c > b) && (b + c > a)
+      if ((a == b) && (a == c) && (b == c))
+        return :equilateral
+      elsif ((a == b) || (a == c) || (b == c))
+        return :isosceles
+    else
+      return :scalene
+    end
+  else
+    raise TriangleError
+  end
+end
+
+class TriangleError < StandardError
+  # error code
+end
 end
